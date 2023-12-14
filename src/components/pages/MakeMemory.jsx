81593b851327ce148f2b6bb9d365c055863ffb1b
SANG\jdsae <jdsaeyqi@naver.com>
2023-12-14 22:33:24 +0900

    feat : 등록 화면 start

---
+++ src/components/pages/MakeMemory.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function MakeMemory(){
+
+    return(
+    
+        <div>
+            <hr className="hr1"/>
+       
+        <div className="box-border w-40 h-40 my-5 bg-yellow"></div> 
+        <hr className="hr1"/>
+        </div>
+        
+            
+            
+
+
+
+    )
+
+}
