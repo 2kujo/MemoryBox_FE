5c9c0332f715c4863227d061ae365bee2d361675
jina0924 <jina092400@gmail.com>
2023-12-14 16:03:20 +0900

    UPDATE | update tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +17,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="text-yellow">Vite + React</h1>
+      <h1 className="text-blue">Vite + React</h1>
+      <div className="text-grey font-light">회색</div>
+      <div className="text-grey font-bold">회색</div>
+      <div className="rounded-sm bg-yellow h-10 w-10"></div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx, html}"],
   theme: {
-    colors: {},
-    fontFamily: {},
+    colors: {
+      yellow: "#FBB901",
+      brown: "#6E6053",
+      grey: "#6E6053",
+      blue: "#287EFF",
+      font1: "#333333",
+      font2: "#838687",
+    },
+    fontFamily: {
+      display: ["KBFGDisplay", "sans-serif"],
+      text: ["KBFGText", "sans-serif"],
+    },
+    borderRadius: {
+      sm: "10px",
+      md: "20px",
+      lg: "30px",
+    },
     extend: {},
   },
   plugins: [],
