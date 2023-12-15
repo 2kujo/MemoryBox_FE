0d407a4cd33f3d11e471ec2e34495ef3a374b5e5
jina0924 <jina092400@gmail.com>
2023-12-15 13:20:42 +0900

    UPDATE | router config

---
+++ src/pages/Memories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Memories() {
+  return (
+    <div>
+      <div>저금통 리스트 화면</div>
+    </div>
+  );
+}
---
+++ src/router.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Intro from "@/pages/Intro";
+import MakeBox from "@/pages/MakeBox";
+import MakeMemory from "@/pages/MakeMemory";
+import Memories from "@/pages/Memories";
+import Memory from "@/pages/Memory";
+import ServiceMain from "@/pages/ServiceMain";
+
+export const RouterInfo = [
+  {
+    path: "/",
+    element: <Intro />,
+  },
+  {
+    path: "/intro-finish",
+    element: <Intro />,
+  },
+  {
+    path: "/make-box",
+    element: <MakeBox />,
+  },
+  {
+    path: "/main",
+    element: <ServiceMain />,
+  },
+  {
+    path: "/memories",
+    element: <Memories />,
+  },
+  {
+    path: "/memories/:memoryId",
+    element: <Memory />,
+  },
+  {
+    path: "/make-memory",
+    element: <MakeMemory />,
+  },
+];
